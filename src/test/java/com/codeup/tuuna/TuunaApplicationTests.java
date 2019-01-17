beac56633ec4dc06d54f5c1ff360c274dfb66d19
davidandstephens <stephens.a.david@gmail.com>
2019-01-17 14:23:47 -0600

    preparing the set up

---
+++ src/test/java/com/codeup/tuuna/TuunaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codeup.tuuna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TuunaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
