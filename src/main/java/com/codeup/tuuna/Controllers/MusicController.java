72d20f8393020ee1fb3ae89bacd125808a85ec9a
davidandstephens <stephens.a.david@gmail.com>
2019-01-22 15:52:25 -0600

    commiting changes so we can look at the site

--- src/main/java/com/codeup/tuuna/Controllers/MusicController.java
+++ src/main/java/com/codeup/tuuna/Controllers/MusicController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class MusicController {
 
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
     @GetMapping("/music")
     public String music() {
         return "music";
