61baa96c6e89bafce30c5ea5e2c9d96f00677320
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2019-01-22 20:51:16 -0600

    creation of initial draft of song controller. Still needs to be tested.

--- src/main/java/com/codeup/tuuna/Controllers/SongController.java
+++ src/main/java/com/codeup/tuuna/Controllers/SongController.java
@@ -1,10 +1,16 @@
 package com.codeup.tuuna.Controllers;
+import com.codeup.tuuna.Models.Song;
+import com.codeup.tuuna.Models.User;
+import com.codeup.tuuna.Repositories.CommentRepository;
+import com.codeup.tuuna.Repositories.RatingRepository;
 import com.codeup.tuuna.Repositories.SongRepository;
 import com.codeup.tuuna.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SongController {
@@ -13,9 +19,16 @@ public class SongController {
 
     private final SongRepository songDao;
 
-    public SongController(UserRepository userDao, SongRepository songDao) {
+    private final CommentRepository commentDao;
+
+    private final RatingRepository ratingDao;
+
+    public SongController(UserRepository userDao, SongRepository songDao, CommentRepository commentDao,
+                          RatingRepository ratingDao) {
         this.userDao = userDao;
         this.songDao = songDao;
+        this.commentDao = commentDao;
+        this.ratingDao = ratingDao;
     }
 
     @GetMapping("/songs")
@@ -31,8 +44,42 @@ public class SongController {
         model.addAttribute("username", songDao.findOne(id).getUser().getEmail());
         model.addAttribute("song", songDao.findOne(id));
         model.addAttribute("id", id);
+        model.addAttribute("comments", songDao.findOne(id).getComments());
+        model.addAttribute("ratings", songDao.findOne(id).getRatings());
+        model.addAttribute("categories", songDao.findOne(id).getCategories());
 
         return "songs/view";
     }
 
+    @GetMapping("/songs/{id}/edit")
+    public String showEditSong(@PathVariable long id, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("post", songDao.findOne(id));
+        return "songs/create";
+    }
+
+    @GetMapping("/songs/create")
+    public String showCreatePost(Model model) {
+        model.addAttribute("post", new Song());
+        return "songs/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createSong(@ModelAttribute Song song, User user) {
+        song.setUser(user);
+        songDao.save(song);
+        return "redirect:/songs";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String editPost(@PathVariable long id, @ModelAttribute Song song) {
+        songDao.save(song);
+        return "redirect:/songs/" + song.getId();
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id, @ModelAttribute Song song) {
+        songDao.delete(id);
+        return "redirect:/songs";
+    }
 }
