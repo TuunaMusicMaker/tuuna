87e00662dfc3912424c7390cab4ef55c7687af62
davidandstephens <stephens.a.david@gmail.com>
2019-01-25 10:42:45 -0600

    hold my beer

--- src/main/java/com/codeup/tuuna/Controllers/HomeController.java
+++ src/main/java/com/codeup/tuuna/Controllers/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -45,6 +46,47 @@ public class HomeController {
             }
             model.addAttribute("recentSongs", recentSongs);
         }
+
+        List<Song> allSongs = Lists.newArrayList(songDao.findAll());
+        List<Song> likedSongs = new ArrayList<>();
+        if (allSongs.size() > 0) {
+            Song holdThis = allSongs.get(0);
+            if (allSongs.size() >= 5) {
+                for (int i = 1; i <= 5; i += 1) {
+                    for (Song song : allSongs) {
+                        if (song.getRatings() != null) {
+                            if (song.getRatings().size() >= holdThis.getRatings().size()) {
+                                holdThis = song;
+                            }
+                        }
+                    }
+                    if (holdThis.getRatings() != null) {
+                        allSongs.remove(holdThis);
+                        likedSongs.add(holdThis);
+                    }
+                }
+            } else {
+                for (int i = 1; i <= allSongs.size(); i += 1) {
+                    for (Song song : allSongs) {
+                        if (song.getRatings() != null) {
+                            System.err.println(song.getRatings().size());
+                            if (song.getRatings().size() >= holdThis.getRatings().size()) {
+                                holdThis = song;
+                            }
+                        }
+                    }
+                    if (holdThis.getRatings() != null) {
+                        allSongs.remove(holdThis);
+                        likedSongs.add(holdThis);
+                    }
+                }
+            }
+        }
+
+        if (likedSongs.size() > 0) {
+            model.addAttribute("likedSongs", likedSongs);
+        }
+
         return "home";
     }
 
