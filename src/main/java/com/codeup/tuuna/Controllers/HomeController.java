0340f6b3f378ffc6e3d266d1141ec2e853603349
davidandstephens <stephens.a.david@gmail.com>
2019-01-25 12:53:20 -0600

    completed search

--- src/main/java/com/codeup/tuuna/Controllers/HomeController.java
+++ src/main/java/com/codeup/tuuna/Controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.codeup.tuuna.Controllers;
+import com.codeup.tuuna.Models.Category;
 import com.codeup.tuuna.Models.Song;
+import com.codeup.tuuna.Models.User;
 import com.codeup.tuuna.Repositories.CommentRepository;
 import com.codeup.tuuna.Repositories.RatingRepository;
 import com.codeup.tuuna.Repositories.SongRepository;
@@ -8,12 +10,11 @@ import com.google.common.collect.Lists;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -100,19 +101,71 @@ public class HomeController {
         return "/search/index";
     }
 
-//    @PostMapping("/search")
-//    public String makeSearch(@RequestParam (name = "search-params") String params, @RequestParam (name = "query") String query) {
-//        List<Song> searchResults;
-//        switch(params) {
-//            case("1"):
-//                break;
-//            case("2"):
-//                break;
-//            case("3"):
-//                break;
-//            default:
-//                break;
-//        }
-//        return"redirect:/search/results";
-//    }
+    @PostMapping("/search")
+    public String makeSearch(Model model,@RequestParam (name = "search-params") String params, @RequestParam (name = "query") String query) {
+        List<Song> searchResults;
+        switch(params) {
+            case("1"):
+                searchResults = new ArrayList<>();
+                List<Song> allSongs = Lists.newArrayList(songDao.findAll());
+                for (Song song : allSongs) {
+                    List<Category> cats =  song.getCategories();
+                    boolean addMe = false;
+                    for (Category cat : cats) {
+                        if (cat.getCategory().contains(query)) {
+                            addMe = true;
+                        }
+                    }
+                    if (addMe) {
+                        searchResults.add(song);
+                    }
+                }
+                model.addAttribute("searchResults", searchResults);
+                break;
+            case("2"):
+                searchResults = songDao.findAllByTitleContaining(query);
+                for(Song result : searchResults) {
+                    System.err.println(result.getTitle());
+                }
+                model.addAttribute("searchResults", searchResults);
+                break;
+            case("3"):
+                searchResults = new ArrayList<>();
+                List<User> users = userDao.findAllByUsernameContaining(query);
+                for (User user : users) {
+                    searchResults.addAll(songDao.findAllByUserId(user.getId()));
+                }
+                model.addAttribute("searchResults", searchResults);
+                break;
+            default:
+                searchResults = songDao.findAllByTitleContaining(query);
+                List<User> usersMaster = userDao.findAllByUsernameContaining(query);
+                for (User user : usersMaster) {
+                    searchResults.addAll(songDao.findAllByUserId(user.getId()));
+                }
+                List<Song> allSongsAllSearch = Lists.newArrayList(songDao.findAll());
+                for (Song song : allSongsAllSearch) {
+                    List<Category> cats =  song.getCategories();
+                    boolean addMe = false;
+                    for (Category cat : cats) {
+                        if (cat.getCategory().contains(query)) {
+                            addMe = true;
+                        }
+                    }
+                    if (addMe) {
+                        searchResults.add(song);
+                    }
+                }
+                Set<Song> holdUp = new HashSet<>(searchResults);
+                searchResults = new ArrayList<>(holdUp);
+                model.addAttribute("searchResults", searchResults);
+                break;
+        }
+        return "search/results";
+    }
+
+    @GetMapping("/search/results")
+    public String showResults(@ModelAttribute (name = "searchResults") ArrayList<Song> searchResults) {
+        return "search/results";
+    }
 }
--- src/main/java/com/codeup/tuuna/Repositories/UserRepository.java
+++ src/main/java/com/codeup/tuuna/Repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.codeup.tuuna.Repositories;
 import com.codeup.tuuna.Models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findAllByUsernameContaining(String username);
 }
